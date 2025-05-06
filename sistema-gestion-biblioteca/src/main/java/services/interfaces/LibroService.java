ad980e50a15ca22161e19c268d3497c38afc91ce
iolmedo <ianitoolmedo12@gmail.com>
2025-05-06 12:56:29 -0300

    Agregando carpeta service e interfaces definidas

---
+++ sistema-gestion-biblioteca/src/main/java/services/interfaces/LibroService.java
@@ -0,0 +1,11 @@
+package services.interfaces;
+import modelo.*;
+import java.util.List;
+
+public interface LibroService {
+    Libro buscarPorIsbn(String isbn);
+    List<Libro> obtenerTodos();
+    Libro guardar(Libro libro);
+    void eliminar(Long id);
+    Libro actualizar(Long id, Libro libro);
+}
