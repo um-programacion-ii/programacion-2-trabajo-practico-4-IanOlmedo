5e7ba9ba567db3476d8005b4190f860ac5fb3963
iolmedo <ianitoolmedo12@gmail.com>
2025-05-07 10:19:16 -0300

    Subiendo implentaciones de Services con logica de negocio

--- sistema-gestion-biblioteca/src/main/java/services/impl/PrestamoServiceImpl.java
+++ sistema-gestion-biblioteca/src/main/java/services/impl/PrestamoServiceImpl.java
@@ -1,4 +1,85 @@
 package services.impl;
+import estados.EstadoLibro;
+import exceptions.LibroNoEncontradoException;
+import exceptions.PrestamoNoEncontradoExcepcion;
+import modelo.*;
+import repository.LibroRepository;
+import repository.PrestamoRepository;
+import services.interfaces.PrestamoService;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
-public class PrestamoServiceImpl {
+
+public class PrestamoServiceImpl implements PrestamoService {
+
+    private final PrestamoRepository prestamoRepository;
+    private final LibroRepository libroRepository;
+
+    public PrestamoServiceImpl(PrestamoRepository prestamoRepository, LibroRepository libroRepository) {
+        this.prestamoRepository = prestamoRepository;
+        this.libroRepository = libroRepository;
+
+    }
+    @Override
+    public Prestamo realizarPrestamo(Libro libro, Usuario usuario){
+        if (libro.getEstadoLibro() != EstadoLibro.DISPONIBLE){
+            throw new LibroNoEncontradoException("El libro "+libro.getTitulo()+" no esta disponible");
+        }
+        Prestamo prestamo = new Prestamo();
+        prestamo.setLibroPrestado(libro);
+        prestamo.setUsuarioPrestatario(usuario);
+        prestamo.setFechaPrestamo(LocalDate.now());
+        prestamo.setFechaDevolucion(null);
+
+        libro.setEstadoLibro(EstadoLibro.PRESTADO);
+        libroRepository.save(libro);
+
+        return prestamoRepository.save(prestamo);
+
+    }
+    @Override
+    public Prestamo obtenerPrestamoPorId(Long id) {
+        return prestamoRepository.findById(id)
+                .orElseThrow(() -> new PrestamoNoEncontradoExcepcion("Prestamo no encontrado con ID: " + id));
+    }
+
+    @Override
+    public List<Prestamo> obtenerTodosPrestamos() {
+        return prestamoRepository.findAll();
+    }
+
+    @Override
+    public List<Prestamo> obtenerPrestamosPorUsuario(Long usuarioId) {
+        return prestamoRepository.findByUsuarioDni(usuarioId);
+    }
+
+    @Override
+    public List<Prestamo> obtenerPrestamosPorLibro(Long libroId) {
+        return prestamoRepository.findByLibroId(libroId);
+    }
+
+    @Override
+    public void marcarComoDevuelto(Long id) {
+        Prestamo prestamo = obtenerPrestamoPorId(id);
+        prestamo.setFechaDevolucion(Optional.of(LocalDate.now()));
+
+        Libro libro = prestamo.getLibroPrestado();
+        libro.setEstadoLibro(EstadoLibro.DISPONIBLE);
+        libroRepository.save(libro); // Actualizar el estado del libro
+        prestamoRepository.save(prestamo);
+    }
+
+    @Override
+    public Prestamo guardarPrestamo(Prestamo prestamo) {
+        return prestamoRepository.save(prestamo);
+    }
+
+    @Override
+    public void eliminarPrestamo(Long id) {
+        if (!prestamoRepository.existsById(id)) {
+            throw new PrestamoNoEncontradoExcepcion("Prestamo no encontrado con ID: " + id);
+        }
+        prestamoRepository.deleteById(id);
+    }
 }
--- sistema-gestion-biblioteca/src/main/java/services/impl/UsuarioServiceImpl.java
+++ sistema-gestion-biblioteca/src/main/java/services/impl/UsuarioServiceImpl.java
@@ -1,4 +1,68 @@
 package services.impl;
+import estados.EstadoUsuario;
+import exceptions.UsuarioNoEncontradoExcepcion;
+import modelo.Usuario;
+import repository.UsuarioRepository;
+import services.interfaces.UsuarioService;
+import java.util.List;
 
-public class UsuarioServiceImpl {
+public class UsuarioServiceImpl implements UsuarioService{
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+    @Override
+    public Usuario obtenerUsuarioPorDni(Long dni) {
+        return usuarioRepository.findByDni(dni)
+                .orElseThrow(()->new UsuarioNoEncontradoExcepcion("Usuario no encontrado por el dni: "+dni));
+    }
+
+    @Override
+    public Usuario obtenerUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(()->new UsuarioNoEncontradoExcepcion("El usuario no fue encontrado por el emial: "+ email));
+    }
+
+    @Override
+    public List<Usuario> obtenerTodosUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario guardarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void eliminarUsuario(Long dni){
+        if (!usuarioRepository.existsByDni(dni)){
+            throw new UsuarioNoEncontradoExcepcion("No se puede eliminar el usuario con el DNI: "+dni+" porque no existe");
+        }
+        usuarioRepository.deleteByDni(dni);
+    }
+
+    @Override
+    public Usuario actualizarUsuario(Long dni, Usuario usuario) {
+        if (!usuarioRepository.existsByDni(dni)){
+            throw new UsuarioNoEncontradoExcepcion("No se puede actualizar el usuario con el DNI: "+dni+" porque no existe");
+        }
+        usuario.setDni(dni);
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void bloquearUsuario(Long dni){
+        Usuario usuario = obtenerUsuarioPorDni(dni);
+        usuario.setEstadoUsuario(EstadoUsuario.BLOQUEADO);
+        usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void activarUsuario(Long dni) {
+        Usuario usuario = obtenerUsuarioPorDni(dni);
+        usuario.setEstadoUsuario(EstadoUsuario.ACTIVO);
+        usuarioRepository.save(usuario);
+    }
+
 }
